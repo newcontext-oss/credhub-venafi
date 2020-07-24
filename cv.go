76aef8c8c70598ed8ebeeaae3b12a8e29fad86ea
marlon <marlon@riseup.net>
2020-07-24 11:50:47 -0400

    Use Sprint instead of Sprintf when not interpolating variables

--- cv.go
+++ cv.go
@@ -250,12 +250,12 @@ func (c CertCompareData) String() string {
 	if c.Left != nil {
 		out += fmt.Sprintf(" Left:%+v ", *c.Left)
 	} else {
-		out += fmt.Sprintf(" Left: nil ")
+		out += fmt.Sprint(" Left: nil ")
 	}
 	if c.Right != nil {
 		out += fmt.Sprintf(" Right:%+v ", *c.Right)
 	} else {
-		out += fmt.Sprintf(" Right: nil ")
+		out += fmt.Sprint(" Right: nil ")
 	}
 	return out
 }
